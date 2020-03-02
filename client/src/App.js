acaab72aa6f3f7417b15700e60ded08692d53ca0
Patty C <patrickcorcoran10@gmail.com>
2020-03-02 17:32:03 -0600

    commit at the breakpoint in the medium article

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import RecipeList from "./components/RecipeList/RecipeList";
 
 function App() {
+  const url = useState(`https://api.myjson.com/bins/t7szj`);
+  const [recipes, setRecipes] = useState([]);
+
+  const fetchRecipe = async () => {
+    const recipeData = await fetch(url);
+    const { recipes } = await recipeData.json();
+    setRecipes(recipes);
+  };
+
+  useEffect(() => {
+    fetchRecipe();
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RecipeList recipes={recipes} />
     </div>
   );
 }
---
+++ client/src/components/RecipeList/RecipeList.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import RecipeList from "../RecipeList/RecipeList";
+import RecipeDetails from "../RecipeDetails/RecipeDetails";
+
+function App() {
+  const url = `https://api.myjson.com/bins/t7szj`;
+  const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const fetchRecipe = async () => {
+    const recipeData = await fetch(url);
+    const { recipes } = await recipeData.json();
+    setRecipes(recipes);
+    setLoading(false);
+  };
+  useEffect(() => {
+    fetchRecipe();
+  });
+  return (
+    <div>
+      {loading ? (
+        <h1 className="text-center">...loading</h1>
+      ) : (
+        <RecipeList recipes={recipes} />
+      )}
+      <RecipeDetails></RecipeDetails>
+    </div>
+  );
+}
+
+export default App;
